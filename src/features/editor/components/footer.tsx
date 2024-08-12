a132c22d981ae54ac5dff29580fcc7ffe7e96962
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-12 14:55:05 +0700

    editor navbar

--- src/features/editor/components/footer.tsx
+++ src/features/editor/components/footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Footer = () => {
   return (
     <footer className='h-[52px] border-t bg-white w-full flex items-center overflow-x-auto z-[49] p-2 gap-x-1 shrink-0 px-4 flex-row-reverse'>
